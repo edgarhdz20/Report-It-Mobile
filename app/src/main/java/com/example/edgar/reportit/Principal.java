80ee64f08b672219a4a60c5b8c60e7354adc7ad3
Edgar <edgar_hg20@hotmail.com>
2016-10-25 00:37:37 -0600

    Se agrego mapa, descripcion y vista mas completa para crear reporte

--- app/src/main/java/com/example/edgar/reportit/Principal.java
+++ app/src/main/java/com/example/edgar/reportit/Principal.java
@@ -9,7 +9,9 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import org.apache.commons.io.output.ByteArrayOutputStream;
@@ -24,8 +26,11 @@ import java.io.InputStream;
 
 public class Principal extends AppCompatActivity {
 
-    Button btnFoto, btnSend;
+    Button btnFoto, btnSend, btnMap;
     ImageView imgvFoto;
+    EditText edtDescription;
+    TextView txtvAddress;
+    int CAMERA_INTENT = 0, MAP_INTENT = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,13 +38,16 @@ public class Principal extends AppCompatActivity {
         setContentView(R.layout.activity_principal);
         btnFoto = (Button)findViewById(R.id.btnFoto);
         btnSend = (Button)findViewById(R.id.btnSend);
+        btnMap = (Button)findViewById(R.id.btnAddress);
         imgvFoto = (ImageView)findViewById(R.id.imgvFoto);
+        edtDescription = (EditText)findViewById(R.id.edtDescription);
+        txtvAddress = (TextView)findViewById(R.id.txtvAddress);
 
         btnFoto.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intento = new Intent("android.media.action.IMAGE_CAPTURE");
-                startActivityForResult(intento,0);
+                startActivityForResult(intento,CAMERA_INTENT);
             }
         });
 
@@ -50,19 +58,40 @@ public class Principal extends AppCompatActivity {
                 upload.execute(((BitmapDrawable)imgvFoto.getDrawable()).getBitmap());
             }
         });
+
+        btnMap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent mapActivity = new Intent(Principal.this, LocationSelect.class);
+                startActivityForResult(mapActivity,MAP_INTENT);
+            }
+        });
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if(requestCode == 0 && resultCode == RESULT_OK){
+        if(requestCode == CAMERA_INTENT && resultCode == RESULT_OK){
             Bundle extras = data.getExtras();
             Bitmap imageBmp = (Bitmap) extras.get("data");
             imgvFoto.setImageBitmap(imageBmp);
         }
+        if(requestCode == MAP_INTENT && resultCode == RESULT_OK){
+
+        }
     }
 
     private class UploadTask extends AsyncTask<Bitmap, Void, Void> {
+        private String description;
+        private int type;
+        private float pos_x, pos_y;
+        private String address;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            description = edtDescription.getText().toString();
+        }
 
         protected Void doInBackground(Bitmap... bitmaps) {
             if (bitmaps[0] == null)
@@ -81,7 +110,7 @@ public class Principal extends AppCompatActivity {
                 MultiPartEntity reqEntity = new MultiPartEntity();
                 reqEntity.addPart("user_id","2");
                 reqEntity.addPart("report_type_id","2");
-                reqEntity.addPart("description","problema");
+                reqEntity.addPart("description", description);
                 reqEntity.addPart("pos_x","28");
                 reqEntity.addPart("pos_y","-106");
                 reqEntity.addPart("address","calle tal");
